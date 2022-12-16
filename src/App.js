3079b7486690c204f852978dca5e6fe724c2c121
falamarcao <falamarcao@gmail.com>
2022-12-16 18:26:30 -0300

    177. Using async / await

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const handleFetchMovies = () => {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) =>
-        response.json().then((json) =>
-          json.results.map((results) => ({
-            id: results.episode_id,
-            title: results.title,
-            openingText: results.opening_crawl,
-            releaseDate: results.release_date,
-          }))
-        )
-      )
-      .then((data) => setMovies(data));
+  const handleFetchMovies = async () => {
+    const response = await fetch("https://swapi.dev/api/films/");
+
+    if (response?.status === 200) {
+      const data = await response.json().then((json) =>
+        json.results.map((data) => ({
+          id: data.episode_id,
+          title: data.title,
+          openingText: data.opening_crawl,
+          releaseDate: data.release_date,
+        }))
+      );
+
+      setMovies(data);
+    }
   };
 
   return (
