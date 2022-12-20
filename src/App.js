b949088533bf1ac14368c549c4481a658d35593f
falamarcao <falamarcao@gmail.com>
2022-12-20 17:03:26 -0300

    refactoring - component folders and renaming as index.jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from "react";
 
-import MoviesList from "./components/MoviesList";
+import MoviesList from "./components/MovieList";
 import "./App.css";
 
 function App() {
